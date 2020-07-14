a917f85d525b6acc220107a706cd3cf0461a25a7
Tyler Butler <tyler@tylerbutler.com>
2020-07-13 23:40:42 -0700

    Add private_dot_local/.keep Add private_dot_local/bin/.keep Update run_once_install-packages.sh.tmpl Update sources/.aliases.sh Update sources/.antigenrc Add sources/.envrc Add symlink_dot_env.tmpl

--- sources/.aliases.sh
+++ sources/.aliases.sh
@@ -11,9 +11,24 @@ alias ll="k --human --almost-all"
 alias lsa="k --human --almost-all"
 alias mkdir="mkdir -p "
 
+# grc overides for ls
+#   `brew install coreutils`
+# if $(gls &>/dev/null)
+# then
+#   alias ls="gls -F --color"
+#   alias l="gls -lAh --color"
+#   alias ll="gls -l --color"
+#   alias la='gls -A --color'
+# fi
+
 # disk usage
 alias df="df -h "
 alias du='du -hd1 | sort -h'
 
 # git
 alias gst="git status -u ."
+
+# Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd="git diff --color | sed 's/^\([^-+ ]*\)[-+ ]/\\1/' | less -r"
+
+# alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'

39fadae8104694b2e9957aa77ad39db627ee0ed3
Tyler Butler <tyler@tylerbutler.com>
2020-07-20 22:32:52 -0700

    powershell profiles

---
+++ sources/.config/powershell/profile.windows.ps1
@@ -0,0 +1,19 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme tylerbu
+Set-Variable -name DefaultUser 'tylerbu'
+
+# Aliases and their functions
+function windows_explorer { explorer . }
+
+New-Alias e windows_explorer
+New-Alias ex explorer
+New-Alias st subl
+Set-Alias git hub

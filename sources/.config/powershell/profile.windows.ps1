8f14ba71017032f73059558a0e732a3d3d3775ab
Tyler Butler <tyler@tylerbutler.com>
2020-07-20 23:24:29 -0700

    powershell profile tweaks

--- sources/.config/powershell/profile.windows.ps1
+++ sources/.config/powershell/profile.windows.ps1
@@ -5,8 +5,8 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 # Prompt
-Import-Module posh-git
-Import-Module oh-my-posh
+# Import-Module posh-git
+# Import-Module oh-my-posh
 Set-Theme tylerbu
 Set-Variable -name DefaultUser 'tylerbu'
 
@@ -17,3 +17,5 @@ New-Alias e windows_explorer
 New-Alias ex explorer
 New-Alias st subl
 Set-Alias git hub
+
+Invoke-Expression (&starship init powershell)

31ab1c13f875627bbfafab9d4e553f86f04a5904
Tyler Butler <tyler@tylerbutler.com>
2020-09-23 15:44:26 -0700

    windows fixes

--- _ps1/install_chocolatey.ps1
+++
@@ -1,4 +0,0 @@
-if ((Test-Path "C:\ProgramData\Chocolatey") -eq $false) {
-  # install Chocolatey
-  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}

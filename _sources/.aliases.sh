bf3efa12afbf15c69b41c28c732891106cd33b7f
Tyler Butler <tyler@tylerbutler.com>
2021-01-22 01:36:17 -0800

    updates

--- _sources/.aliases.sh
+++ _sources/.aliases.sh
@@ -15,13 +15,31 @@ alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied t
 
 # listings and navigation
 alias cdr="cd-gitroot"
+# general use
+alias ls='exa'                                                          # ls
+alias l='exa -lbF --git'                                                # list, size, type, git
+alias ll='exa -lbGF --git'                                             # long list
+alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+
+# specialty views
+alias lS='exa -1'                                                              # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
+
 alias k="k --human"
 alias kl="k --human --almost-all"
-alias l="ls "
-alias ll="k --human --almost-all"
-alias ls="ls --color -o "
-alias lsa="\ls --color -oA "
+#alias l="ls "
+#alias ll="exa -l"
+#alias ls="ls --color -o "
+#alias lsa="\ls --color -oA "
 alias mkdir="mkdir -p "
+
+# list only directories
+alias lsd='ls -d */'
+# list only files
+#alias lsf="ls -rtF | grep -v '.*/'"
+#alias l='ls -l'
 
 # grc overides for ls; brew install coreutils
 # if $(gls &>/dev/null)

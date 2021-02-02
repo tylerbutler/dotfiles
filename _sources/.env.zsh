8349ab05e6a9c1163f826e47226b4ed10321d638
Tyler Butler <tyler@tylerbutler.com>
2021-02-01 23:32:57 -0800

    editors

--- _sources/.env.zsh
+++ _sources/.env.zsh
@@ -6,7 +6,7 @@ export BAT_CONFIG_PATH="$HOME/.bat.conf"
 # Preferred editor for local and remote sessions
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
     SESSION_TYPE=remote/ssh
-    export EDITOR="nano"
+    export EDITOR="micro"
 else
     case $(ps -o comm= -p $PPID) in
         sshd|*/sshd) SESSION_TYPE=remote/ssh;;

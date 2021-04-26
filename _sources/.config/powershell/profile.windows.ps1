566084ed882f3e79328824f7c10c186d59d923fe
Tyler Butler <tyler@tylerbutler.com>
2021-04-26 10:04:07 -0700

    updates

--- _sources/.config/powershell/profile.windows.ps1
+++ _sources/.config/powershell/profile.windows.ps1
@@ -5,13 +5,14 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 Import-Module PowerShellGet
+Import-Module -Name Terminal-Icons
 
 Write-Output "Sourcing aliases..."
 . $HOME/.aliases.ps1
 
 # Prompt
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme jandedobbeleer
+Set-PoshPrompt -Theme ~/.tylerbu.omp.json
 
 Set-Variable -name DefaultUser 'tylerbu'
 

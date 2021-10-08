4cda08406ad210e0b3279d33614dc755df7168e5
Tyler Butler <tyler@tylerbutler.com>
2021-10-07 22:00:20 -0700

    psreadline

--- _sources/.config/powershell/profile.shared.ps1
+++ _sources/.config/powershell/profile.shared.ps1
@@ -12,3 +12,23 @@ Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
 })
+
+#use PSReadLine only for PowerShell and VS Code
+if ($host.Name -eq 'ConsoleHost' -or $host.Name -eq 'Visual Studio Code Host' ) {
+    #ensure the correct version is loaded
+    Import-Module PSReadline -RequiredVersion 2.2.0
+    #ListView currently works only with -EditMode Windows properly
+    Set-PSReadLineOption -EditMode Windows
+    if ($host.Version.Major -eq 7){
+        #only PS 7 supports HistoryAndPlugin
+        Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    }
+    else{
+        #use history as the prediction source on 5.1
+        Set-PSReadLineOption -PredictionSource History
+    }
+    #add background color to the prediction preview
+    Set-PSReadLineOption -Colors @{InlinePrediction = "$([char]0x1b)[36;7;238m]"}
+    #change the key to accept suggestions (default is right arrow)
+    Set-PSReadLineKeyHandler -Function AcceptSuggestion -Key 'ALT+r'
+}

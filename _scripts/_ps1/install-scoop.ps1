51a791549f1de0b35171904644339ccc08ac6ef9
Tyler Butler <tyler@tylerbutler.com>
2021-11-26 18:36:28 -0800

    updates

--- _scripts/_ps1/install-scoop.ps1
+++ _scripts/_ps1/install-scoop.ps1
@@ -1,3 +1,4 @@
 Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
 
 scoop bucket add extras
+scoop bucket add twpayne https://github.com/twpayne/scoop-bucket
--- _sources/.config/powershell/prompt.ps1
+++ _sources/.config/powershell/prompt.ps1
@@ -1,7 +1,8 @@
 # Prompt
 Write-Output "Setting prompt..."
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme $HOME/.tylerbu.omp.json
+# Set-PoshPrompt -Theme $HOME/.tylerbu.omp.json
+Set-PoshPrompt -Theme $HOME/.headline.omp.json
 
 Set-Variable -name DefaultUser 'tylerbu'
 

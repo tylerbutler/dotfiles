39101afedb098e8cca2f62b393bc5ebc394c599c
Tyler Butler <tylerbu@microsoft.com>
2021-12-07 18:10:14 -0800

    updates

--- private_dot_config/powershell/prompt.ps1
+++ private_dot_config/powershell/prompt.ps1
@@ -1,8 +1,10 @@
 # Prompt
 Write-Output "Setting prompt..."
-Import-Module oh-my-posh
+# Import-Module oh-my-posh
+oh-my-posh --init --shell pwsh --config "$HOME/.headline.omp.json" | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config "$HOME/.shanselman.omp.json" | Invoke-Expression
 # Set-PoshPrompt -Theme $HOME/.tylerbu.omp.json
-Set-PoshPrompt -Theme $HOME/.headline.omp.json
+# Set-PoshPrompt -Theme $HOME/.headline.omp.json
 # Set-PoshPrompt -Theme $HOME/sample.omp.json
 
 Set-Variable -name DefaultUser 'tylerbu'

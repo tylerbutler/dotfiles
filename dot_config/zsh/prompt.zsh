65a5bc5fee5dedaac5a75841c349b8333e4ef89d
Tyler Butler <tyler@tylerbutler.com>
2024-01-20 17:39:27 +0000

    prompt

--- dot_config/zsh/prompt.zsh
+++ dot_config/zsh/prompt.zsh
@@ -1,19 +1,30 @@
 # PROMPT CONFIG
 
-# eval "$(starship init zsh)"
+headlinePrompt() {
+	# headline zsh theme
+	source ~/.config/themes/zsh/headline.zsh-theme
+	HEADLINE_DO_GIT_STATUS_COUNTS=true
+	HEADLINE_LINE_MODE="on"
+	HEADLINE_STATUS_TO_STATUS=""
+	HEADLINE_USER_BEGIN=""	
+}
+
+starshipPrompt() {
+	if command -v starship >/dev/null 2>&1; then
+		# starship
+		eval "$(starship init zsh)"	
+	else
+		headlinePrompt
+	fi
+}
 
 if [ "$CODESPACES" = true ]; then
-	# do something
+	headlinePrompt
+else
+	starshipPrompt
 fi
 
 # oh-my-posh
 # eval "$(oh-my-posh --init --shell zsh --config ~/.config/oh-my-posh/headline-tylerbu.omp.json)"
-
-# headline zsh theme
-source ~/.config/themes/zsh/headline.zsh-theme
-HEADLINE_DO_GIT_STATUS_COUNTS=true
-HEADLINE_LINE_MODE="on"
-HEADLINE_STATUS_TO_STATUS=""
-HEADLINE_USER_BEGIN=""
 
 POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true

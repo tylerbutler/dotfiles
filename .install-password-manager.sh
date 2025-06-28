55cad109d2642f8169b9884827a78ad887f367c0
Tyler Butler <tyler@tylerbutler.com>
2025-06-27 20:38:02 -0700

    updates

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type bw >/dev/null 2>&1 && exit
+
 npm install -g @bitwarden/cli
 
 bw config server https://vaultwarden.btlr.org

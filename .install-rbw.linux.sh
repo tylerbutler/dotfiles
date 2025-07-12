7b68ee43ccc9ede3144ce41cea5d211c611a1b4d
Tyler Butler <tyler@tylerbutler.com>
2025-07-12 15:02:13 -0700

    linux install

--- .install-rbw.linux.sh
+++ .install-rbw.linux.sh
@@ -9,7 +9,7 @@ arm64)
     sh "$CHEZMOI_SOURCE_DIR/.install-rustup.sh"
     cargo install rbw
     ;;
-amd64)
+amd64|x86_64)
     # Download the .deb package
     tmp_deb="/tmp/rbw_1.9.0_amd64.deb"
     curl -L -o "$tmp_deb" "https://git.tozt.net/rbw/releases/deb/rbw_1.9.0_amd64.deb"

2c05388caa933325dd263abc9f39fc8b216ea75b
Tyler Butler <tyler@tylerbutler.com>
2026-07-25 18:27:05 -0700

    updates

--- dot_config/powershell/profile.linux.ps1
+++ dot_config/powershell/profile.linux.ps1
@@ -1,3 +1,5 @@
 . $HOME/.config/powershell/profile.shared.ps1
 
 . $HOME/.config/powershell/prompt.ps1
+
+if (Get-Command wt -ErrorAction SilentlyContinue) { Invoke-Expression (& wt config shell init powershell | Out-String) }
